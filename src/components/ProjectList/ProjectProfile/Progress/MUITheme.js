9046a3e9617280cec9eeea1ce075c7f9a4958ace
y1141335509 <y1141335509@gmail.com>
2023-11-06 23:23:39 -0800

    changed leads

--- src/components/ProjectList/ProjectProfile/Progress/MUITheme.js
+++ src/components/ProjectList/ProjectProfile/Progress/MUITheme.js
@@ -3,8 +3,15 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
   typography: {
     fontFamily: 'Roboto, sans-serif',
-    
   },
+  palette: {
+    primary: {
+      main: 'rgb(93, 100, 101)',
+    },
+    secondary: {
+      main: '#F0ECEC',
+    }
+  }
 });
 
 
